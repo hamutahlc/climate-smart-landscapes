a88f5b5adbd79af94fd5205aad820380da6475b2
Hamutahl Cohen <43823923+hamutahlc@users.noreply.github.com>
2022-08-23 10:45:25 -0400

    dataprep draft files

---
+++ dataPrep/libraries.R
@@ -0,0 +1,8 @@
+rm(list=ls())
+## install and load libraries for all analyses
+install.packages("vegan")
+install.packages("piecewiseSEM")
+install.packages("lme4")
+install.packages("lmerTest")
+install.packages("brms")
+
