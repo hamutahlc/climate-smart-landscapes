d5b5fad96fd563cc485b0bdcfebcaaea1eb447cf
Hamutahl Cohen <43823923+hamutahlc@users.noreply.github.com>
2025-02-06 20:46:02 -0800

    I added code to plot clusters by obstacles perceived

--- analyses/CharacterizingClustersandPlotting.R
+++ analyses/CharacterizingClustersandPlotting.R
@@ -36,6 +36,10 @@ clusterdata$Q19_PesticdeLicense<-as.factor(clusterdata$Q19_PesticdeLicense)
 clusterdata$Q18_AnyProfAssoc<-as.factor(clusterdata$Q18_AnyProfAssoc)
 clusterdata$Q16_General_info_needed<-as.factor(clusterdata$Q16_General_info_needed)
 
+clusterdata$Q13_4_Engagement<-as.factor(clusterdata$Q13_4_Engagement)
+clusterdata$Q13_5_Engagement<-as.factor(clusterdata$Q13_5_Engagement)
+
+clusterdata$Q14_BiggestObstacle<-as.factor(clusterdata$Q14_BiggestObstacle)
 
 
 #run chi sq tests
@@ -46,6 +50,7 @@ table(clusterdata$Cluster, clusterdata$Q20_Sex)
 sex
 
 #check expected frequencies
+#Compare observed and expected cell counts: which cells have more or less observations than would be expected if H0 were true?
 sex$expected
 #if more than 20% of cells have expected frequencies < 5, 
 #we would need to use Fisher's exact test because applying approximation method is inadequate
@@ -59,6 +64,11 @@ sex$expected
 cramersv(clusterdata[c("Cluster", "Q20_Sex")])
 
 #RACE
+#if you drop the "Other" category of race, not significant, so not including
+#clusterdata[clusterdata$Q22_Race=="Other"] <- NA
+
+clusterdata$Q22_Race <-as.factor(clusterdata$Q22_Race)
+
 race<-chisq.test(clusterdata$Cluster, clusterdata$Q22_Race, correct=FALSE)
 table(clusterdata$Cluster, clusterdata$Q22_Race)
 race
@@ -84,7 +94,7 @@ license<-chisq.test(clusterdata$Cluster, clusterdata$Q19_PesticdeLicense, correc
 table(clusterdata$Cluster, clusterdata$Q19_PesticdeLicense)
 license
 
-ethnicity$expected
+
 
 #HOLD A PROF ASSOCIATION
 prof<-chisq.test(clusterdata$Cluster, clusterdata$Q18_AnyProfAssoc, correct=FALSE)
@@ -132,10 +142,33 @@ table(clusterdata$Cluster, clusterdata$Q16_General_info_needed)
 info
 
 info$expected
-
-
 cramersv(clusterdata[c("Cluster", "Q16_General_info_needed")])
 
+
+#I will attend climate change extension even if not required for my business operations and licensing
+attend<-chisq.test(clusterdata$Cluster, clusterdata$Q13_5_Engagement, correct=FALSE)
+table(clusterdata$Cluster, clusterdata$Q13_5_Engagement)
+attend
+
+attend$expected
+cramersv(clusterdata[c("Cluster", "Q13_5_Engagement")])
+
+#I trust University Extension program and staff to provide unbiased climate change education
+trust<-chisq.test(clusterdata$Cluster, clusterdata$Q13_4_Engagement, correct=FALSE)
+trust
+cramersv(clusterdata[c("Cluster", "Q13_4_Engagement")])
+
+trust$expected
+table(clusterdata$Cluster, clusterdata$Q13_4_Engagement)
+
+
+# Biggest obstacle
+obstacle<-chisq.test(clusterdata$Cluster, clusterdata$Q14_BiggestObstacle, correct=FALSE)
+obstacle
+cramersv(clusterdata[c("Cluster", "Q14_BiggestObstacle")])
+
+obstacle$expected
+table(clusterdata$Cluster, clusterdata$Q14_BiggestObstacle)
 
 
 
@@ -153,11 +186,44 @@ mantelhaen.test(tab)
 
 #PLOTTING
 
-#remove NAs from the graphs
+#remove NAs from all data
 
-unique(clusterdata$Cluster)
-clusterdataNA.omit <- na.omit(clusterdata)
-unique(clusterdataNA.omit$Cluster)
+#unique(clusterdata$Cluster)
+#clusterdataNA.omit <- na.omit(clusterdata)
+#unique(clusterdataNA.omit$Cluster)
+
+#can we just remove rows where NAs were in the cluster column or the Q14_BiggestObstacle column
+data <- clusterdata %>% 
+            select(Q14_BiggestObstacle, Cluster) %>% 
+            drop_na() 
+
+#use wchich dataset you need from the two above
+
+clusters <- c("High Involvement, \nHigh Knolwedge", "Low Involvement, \nHigh Knolwedge",  "Low Involvement, \nModerate Knolwedge")
+
+Q14<- ggplot(data) +
+  aes(x = Cluster, fill = Q14_BiggestObstacle) +
+  geom_bar() +
+  scale_fill_manual(values=wes_palette(n=5, name="Darjeeling2"), 
+                    labels = c("Cost of tools & practices", "Lack of demand", "Lack of tools & practices", "Lack of knowledge", "Not a priority for me")) +
+  coord_flip() +
+  ylab("Number of Individuals") +
+  theme_pubr() + 
+  scale_x_discrete(labels= clusters) +
+  theme(legend.direction = "vertical",
+        legend.position = "right") +
+  guides(fill = guide_legend(title = "Biggest obstacles to implementing \n climate-friendly practices")) 
+  
+  
+ggsave("figs/Q14BiggestObstacle.pdf",
+       height=4, width=12)
+
+
+
+ggplot(heatmap, aes(x = X2, y = X1, fill = value)) +
+  geom_tile() + 
+  scale_fill_gradientn(colours = pal) 
+
 
 
 ggplot(clusterdataNA.omit) +
@@ -167,6 +233,13 @@ ggplot(clusterdataNA.omit) +
 ggplot(clusterdataNA.omit) +
   aes(x = Cluster, fill = Q16_General_info_needed,) +
   geom_bar() 
+
+ggplot(clusterdataNA.omit) +
+  aes(x = Cluster, fill = Q15_Format,) +
+  geom_bar() 
+
+
+
 
 
 
